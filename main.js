4e96299bc89b04d8cab9aa1308ed76e2cd13c35d
hy2999la <ansonlam82@gmail.com>
2021-02-15 20:19:48 -0500

    refactored lobby

--- main.js
+++ main.js
@@ -1,117 +1,25 @@
 require('dotenv').config();
 
+const Lobby = require('./src/lobby');
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const mentions = ['apex', 'apex-test'];
-const MAX_LOBBY = 3;
-
-/** @type {Discord.Message} */
-let lobbyMessage = null;
-
-/** @type {Discord.ReactionCollector} */
-let collector = null;
-let lobbyUserNames = {};
-
-/// Helper
-
-const filter = (reaction) => reaction.emoji.name === '👍';
-
-const addUserToLobby = (user) => {
-  if (!lobbyUserNames.hasOwnProperty(user.displayName)) {
-    console.log(`Adding ${user.displayName} to current lobby`);
-    lobbyUserNames[user.displayName] = user.id;
-    return true;
-  } else {
-    console.log(`${user.displayName} already exist in lobby`);
-    return false;
-  }
-};
-
-const onReact = (r, user) => {
-  const msg = r.message.toString();
-  const foundGuildMember = r.message.guild.member(user);
-  if (addUserToLobby(foundGuildMember)) {
-    if (Object.keys(lobbyUserNames).length == MAX_LOBBY) {
-      console.log(`Current Lobby is now full, deleting lobby message!`);
-      lobbyMessage.delete();
-      let startMsg = 'Game is starting: ';
-      for (let user in lobbyUserNames) {
-        startMsg += `<@${lobbyUserNames[user]}> /`
-      }
-      startMsg = startMsg.slice(0, -2);
-      startMsg += '. GL';
-      lobbyMessage.channel.send(startMsg);
-      console.log('=============================');
-
-      resetCurrLobby();
-    } else {
-      const newMsg = msg.replace('Free', foundGuildMember.displayName);
-      lobbyMessage.edit(newMsg);
-    }
-  }
-};
-
-const onRemoveReact = (r, user) => {
-  const msg = r.message.toString();
-  const foundGuildMember = r.message.guild.member(user);
-  if (msg.indexOf(foundGuildMember.displayName) > 0) {
-    console.log(`Removing ${foundGuildMember.displayName} from current lobby`);
-    delete lobbyUserNames[foundGuildMember.displayName];
-    const newMsg = msg.replace(foundGuildMember.displayName, 'Free');
-    lobbyMessage.edit(newMsg);
-  } else {
-    console.log(`${foundGuildMember.displayName} is not in the lobby`);
-  }
-}
-
-const resetCurrLobby = () => {
-  lobbyMessage = null;
-  collector.removeAllListeners();
-  collector = null;
-  lobbyUserNames = [];
-};
-
-const createLobby = async (msg, user) => {
-  if (lobbyMessage != null && !lobbyMessage.deleted) {
-    // There's a lobby already
-    console.log('Cancelling lobby');
-    console.log('=============================');
-
-    lobbyMessage.edit('Lobby has been cancelled');
-    lobbyMessage.reactions.removeAll();
-
-    resetCurrLobby();
-  }
-
-  console.log('Creating lobby');
-  console.log('=============================');
-  
-  lobbyMessage = msg;
-  addUserToLobby(user);
-  lobbyMessage = await msg.channel.send(
-    `Creating a Lobby...\n1. ${user.displayName}\n2. Free \n3. Free\nReact to join this lobby`
-  );
-
-  await lobbyMessage.react('👍');
-
-  collector = lobbyMessage.createReactionCollector(filter, { dispose: true });
-
-  collector.on('collect', onReact);
-  collector.on('remove', onRemoveReact);
-};
-
+let lobby;
 
 /// Client
 
 client.once('ready', () => {
   console.log('Bot is ready');
+  lobby = new Lobby();
 });
 
 client.on('message', async (message) => {
+  const mentions = Lobby.getMentions();
   if (message.mentions.roles.some(r => mentions.indexOf(r.name) >= 0) || message.content === '!start') {
     const user = message.member;
-    createLobby(message, user);
+
+    lobby.initLobby(message, user);
   }
 });
 
--- src/lobby/index.js
+++ src/lobby/index.js
@@ -1,13 +1,100 @@
 const config = require('./config.json');
+const constants = require('../utils/constants');
 
 class Lobby {
   constructor(lobbyMessage = null) {
     this.lobbyMessage = lobbyMessage;
     this.collector = null;
-    this.lobbyUsers = null;
+    this.lobbyUsers = [];
   }
 
+  // Initializes lobby with the message and the user who mentioned the bot
+  async initLobby(message, user) {
+    if (this.lobbyMessage != null && !this.lobbyMessage.deleted) {
+      // There's a lobby already
+      console.log('Cancelling lobby');
+      console.log('=============================');
   
+      this.lobbyMessage.edit('Lobby has been cancelled');
+      this.lobbyMessage.reactions.removeAll();
+  
+      this.clearLobby();
+    }
+  
+    console.log('Creating lobby');
+    console.log('=============================');
+    
+    this.addUser(user);
+    this.lobbyMessage = await message.channel.send(
+      `Creating a Lobby...\n1. ${user.displayName}\n2. Free \n3. Free\nReact to join this lobby`
+    );
+  
+    await this.lobbyMessage.react('👍');
+
+    this.collector = this.lobbyMessage.createReactionCollector(() => true, { dispose: true });
+
+    this.collector.on('collect', this.onReact);
+    this.collector.on('remove', this.onRemoveReact);
+  }
+
+  addUser(user) {
+    if (!this.lobbyUsers.hasOwnProperty(user.displayName)) {
+      console.log(`Adding ${user.displayName} to current lobby`);
+      this.lobbyUsers[user.displayName] = user.id;
+      return true;
+    } else {
+      console.log(`${user.displayName} already exist in lobby`);
+      return false;
+    }
+  }
+
+  clearLobby() {
+    this.lobbyMessage = null;
+    this.collector.removeAllListeners();
+    this.collector = null;
+    this.lobbyUserNames = [];
+  }
+
+  onReact(r, user) {
+    const msg = r.message.toString();
+    const foundGuildMember = r.message.guild.member(user);
+    if (this.addUser(foundGuildMember)) {
+      if (Object.keys(this.lobbyUsers).length == constants.lobbySize.apex) {
+        console.log(`Current Lobby is now full, deleting lobby message!`);
+        this.lobbyMessage.delete();
+        let startMsg = 'Game is starting: ';
+        for (let user in this.lobbyUsers) {
+          startMsg += `<@${this.lobbyUsers[user]}> /`
+        }
+        startMsg = startMsg.slice(0, -2);
+        startMsg += '. GL';
+        this.lobbyMessage.channel.send(startMsg);
+        console.log('=============================');
+  
+        resetCurrLobby();
+      } else {
+        const newMsg = msg.replace('Free', foundGuildMember.displayName);
+        this.lobbyMessage.edit(newMsg);
+      }
+    }
+  }
+
+  onRemoveReact(r, user) {
+    const msg = r.message.toString();
+    const foundGuildMember = r.message.guild.member(user);
+    if (msg.indexOf(foundGuildMember.displayName) > 0) {
+      console.log(`Removing ${foundGuildMember.displayName} from current lobby`);
+      delete this.lobbyUsers[foundGuildMember.displayName];
+      const newMsg = msg.replace(foundGuildMember.displayName, 'Free');
+      this.lobbyMessage.edit(newMsg);
+    } else {
+      console.log(`${foundGuildMember.displayName} is not in the lobby`);
+    }
+  }
+
+  static getMentions() {
+    return config.mentions;
+  }
 }
 
 module.exports = Lobby;
