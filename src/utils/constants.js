da9f2a088829277f5034b823fa909eb7e855c9fc
Anson Lam <hy2999la@uwaterloo.ca>
2021-08-17 14:43:42 -0400

    added environment sensative constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,26 +1,5 @@
-export default {
-	APEX: {
-		LOBBY_SIZE: 1,
-		ROLE_ID: '808578063445131265',
-		MESSAGE:
-`{ROLE_ID}
-Creating Lobby...
-**
-1. -Free-
-2. -Free-
-3. -Free-
-**
-`,
-	},
-	LOBBY: {
-		ALLOWED_CHANNELS: ['808419512390516786', '808576788791427092'],
-	},
-	YAHOOFINANCE: {
-		queryUrl: 'https://query1.finance.yahoo.com/v7/finance/quote',
-		webpage: 'https://finance.yahoo.com/quote',
-	},
-	ERRORCODES: {
-		TICKER_NOT_FOUND: 'TICKER_NOT_FOUND',
-		ERROR_FETCHING_TICKER_DATA: 'ERROR_FETCHING_TICKER_DATA',
-	},
-};
+import constantsDev from './constants.dev.js';
+import constantsProd from './constants.prod.js';
+
+const constants = process.env.ENVIRONMENT == 'PROD' ? constantsProd : constantsDev;
+export default constants;
